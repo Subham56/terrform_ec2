6017a00aa2fefd0c326d17d75c98e67ff72fa730
subham56 <subham56@gmail.com>
2023-10-30 11:12:52 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "web" {
+    ami= var.instance_ami
+    instance_type = var.instance_type
+    
+    tags = {
+    name = "terraform_demo"
+} 
+}
+
+terraform {
+    backend "s3" {
+        bucket = "terraform-bucket-demo1"
+        region = "ap-south-1"
+        key = "terraform.tfstate"
+        dynamodb_table = "state-lock"
+
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable instance_ami {
+    default = "ami-0da59f1af71ea4ad2"
+}
+
+variable instance_type {
+    default = "t2.micro"
+}
+
